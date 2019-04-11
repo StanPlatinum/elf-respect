f373b89a992bc0daf4a25112bdbe79700e4ab19a
Weijie Liu <weijliu@bio-sgx.soic.indiana.edu>
2019-04-11 16:34:44 -0400

    upload all binary and .so

---
+++ minimal_example/App/Enclave_u.c
@@ -0,0 +1,52 @@
+#include "Enclave_u.h"
+#include <errno.h>
+
+typedef struct ms_Ecall_SomeDataProcessing_t {
+	char* ms_buf;
+	size_t ms_len;
+} ms_Ecall_SomeDataProcessing_t;
+
+typedef struct ms_Ecall_MaliciousDataProcessing_t {
+	uint64_t ms_p_inside;
+} ms_Ecall_MaliciousDataProcessing_t;
+
+typedef struct ms_Ocall_PrintString_t {
+	char* ms_str;
+} ms_Ocall_PrintString_t;
+
+static sgx_status_t SGX_CDECL Enclave_Ocall_PrintString(void* pms)
+{
+	ms_Ocall_PrintString_t* ms = SGX_CAST(ms_Ocall_PrintString_t*, pms);
+	Ocall_PrintString((const char*)ms->ms_str);
+
+	return SGX_SUCCESS;
+}
+
+static const struct {
+	size_t nr_ocall;
+	void * table[1];
+} ocall_table_Enclave = {
+	1,
+	{
+		(void*)Enclave_Ocall_PrintString,
+	}
+};
+sgx_status_t Ecall_SomeDataProcessing(sgx_enclave_id_t eid, char* buf, size_t len)
+{
+	sgx_status_t status;
+	ms_Ecall_SomeDataProcessing_t ms;
+	ms.ms_buf = buf;
+	ms.ms_len = len;
+	status = sgx_ecall(eid, 0, &ocall_table_Enclave, &ms);
+	return status;
+}
+
+sgx_status_t Ecall_MaliciousDataProcessing(sgx_enclave_id_t eid, uint64_t p_inside)
+{
+	sgx_status_t status;
+	ms_Ecall_MaliciousDataProcessing_t ms;
+	ms.ms_p_inside = p_inside;
+	status = sgx_ecall(eid, 1, &ocall_table_Enclave, &ms);
+	return status;
+}
+
---
+++ minimal_example/Enclave/Enclave_t.h
@@ -0,0 +1,29 @@
+#ifndef ENCLAVE_T_H__
+#define ENCLAVE_T_H__
+
+#include <stdint.h>
+#include <wchar.h>
+#include <stddef.h>
+#include "sgx_edger8r.h" /* for sgx_ocall etc. */
+
+#include "user_types.h"
+
+#include <stdlib.h> /* for size_t */
+
+#define SGX_CAST(type, item) ((type)(item))
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void Ecall_SomeDataProcessing(char* buf, size_t len);
+void Ecall_MaliciousDataProcessing(uint64_t p_inside);
+
+sgx_status_t SGX_CDECL Ocall_PrintString(const char* str);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif
