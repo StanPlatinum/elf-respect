b823d499abe758c2b7a6d725246338df557a3f69
lpjlwj <lpjlwjlpjlwj@126.com>
2019-06-15 23:24:29 -0400

    test musl libc

--- capstone-checker/checker_in_enclave/Enclave/Enclave.cpp
+++ capstone-checker/checker_in_enclave/Enclave/Enclave.cpp
@@ -39,13 +39,36 @@ int Ecall_entry(char *filename) {
                 PrintDebugInfo("Cannot open file %s\n", filename);
                 return -1;
         }
-#if 0
         e = elf_begin(fd, ELF_C_READ_MMAP, NULL);
         if (e == NULL) {
                 PrintDebugInfo("elf_begin failed\n");
                 return -1;
         }
-#endif
+
+        if (elf_getshdrstrndx(e, &shstrndx) != 0){
+                PrintDebugInfo("Cannot get string section\n");
+                return -1;
+        }
+
+        char* name;
+        Elf64_Off textOff = 0;
+        Elf64_Xword textSize;
+        Elf64_Addr textAddr;
+        while ( (scn = elf_nextscn(e, scn)) != NULL) {
+                shdr64 = elf64_getshdr(scn);
+                name = elf_strptr(e, shstrndx, shdr64->sh_name);
+                if (name == NULL) {
+                        PrintDebugInfo("elf_strptr returns NULL\n");
+                        continue;
+                }
+                if (strcmp(name, ".text") == 0) {
+                        textOff = shdr64->sh_offset;
+                        textSize = shdr64->sh_size;
+                        textAddr = shdr64->sh_addr;
+                        break;
+                }
+        }
+
 	close(fd);
 	return 0;
 }
