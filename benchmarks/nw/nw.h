60bc4415ed737deb058a5acf25aa77784cbf97bb
StanPlatinum <lpjlwjlpjlwj@126.com>
2019-07-10 21:55:45 -0400

    prepare nw

---
+++ benchmarks/nw/nw.h
@@ -0,0 +1,10 @@
+#include <string>
+#include <algorithm>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define NW_DEBUG 1
+#define INPUT_MAX_SIZE 500
+#define AL_MAX_SIZE 1000
+ 
+using namespace std; 
