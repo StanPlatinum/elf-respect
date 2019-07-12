970ab2365f10538704aa813af398e241d0c1b507
Weijie Liu <lpjlwjlpjlwj@126.com>
2019-07-11 21:03:58 -0400

    Update Enclave.cpp

--- benchmarks/stock_sgx/Enclave/Enclave.cpp
+++ benchmarks/stock_sgx/Enclave/Enclave.cpp
@@ -24,6 +24,25 @@ void PrintDebugInfo(const char *fmt, ...)
 #include <memory>
 
 #include <vector>
+std::vector<std::string> split(std::string str,std::string pattern)
+{
+	std::string::size_type pos;
+     std::vector<std::string> result;
+     str+=pattern;//扩展字符串以方便操作
+     int size=str.size();
+ 
+     for(int i=0; i<size; i++)
+     {
+         pos=str.find(pattern,i);
+         if(pos<size)
+         {
+             std::string s=str.substr(i,pos-i);
+             result.push_back(s);
+             i=pos+pattern.size()-1;
+         }
+     }
+     return result;
+ }
 
 float calcVolatility(float spotPrice, int timesteps)
 {
