be195510c7d3ee2ae27e093bca07821ee7693032
StanPlatinum <lpjlwjlpjlwj@126.com>
2019-07-14 21:55:04 -0400

    add entry while compiling

--- loader/test-program/bm_hello.c
+++ loader/test-program/bm_hello.c
@@ -1,8 +1,8 @@
 /* a very simple test program */
 
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include "enclave.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "enclave.h"
 
 int global_test = 0;
 
@@ -11,9 +11,5 @@ void enclave_main()
 	int j = 0;
 	//puts("hello");
 	for (int i = 0; i < 10; i++)	j++;
-	//enclave_exit();
-}
-
-int main(void){
-	enclave_main();
+	enclave_exit();
 }
