2fee9480e4e38967eab90fa31dabf01f2d0724a0
StanPlatinum <lpjlwjlpjlwj@126.com>
2019-08-19 01:09:47 -0400

    try w/o malloc for enclave end

--- shadow-stack/Enclave/Enclave.cpp
+++ shadow-stack/Enclave/Enclave.cpp
@@ -40,6 +40,35 @@ void PrintDebugInfoOutside3(void)
 
 
 void try2write_stackbase(){
+
+	PrintDebugInfo("starting...\n");
         void *this_enclave_base = get_enclave_base();
         size_t this_enclave_size = get_enclave_size();
+	
+	PrintDebugInfo
+	("base: 0x%x, size: 0x%x\n", this_enclave_base, this_enclave_size);
+
+	size_t this_enclave_end = get_enclave_end();
+	PrintDebugInfo("end: 0x%x\n", this_enclave_end);
+
+	size_t maxAddr = (size_t)this_enclave_base + (size_t)this_enclave_size;
+	PrintDebugInfo("maxAddr: 0x%x\n", this_enclave_end);
+	
+	unsigned char *al;
+	PrintDebugInfo("allocating...\n");
+	size_t al_num = (size_t)this_enclave_size / 4000;
+	PrintDebugInfo("al_num: 0x%lx\n", al_num);
+	al = (unsigned char *)malloc(al_num * (sizeof(unsigned char)));
+	PrintDebugInfo("initializing...\n");
+	for (int i = 0; i < al_num; i++){
+		al[i] = '1';
+	}
+	PrintDebugInfo("al[end - 1]: %c\n", al[this_enclave_size - 1]);
+	
+	size_t* test;
+	test = (size_t *)(this_enclave_end);
+	PrintDebugInfo("trying to write end...\n");
+	//*test = 10;
+
+	PrintDebugInfo("finishing...\n");
 }
