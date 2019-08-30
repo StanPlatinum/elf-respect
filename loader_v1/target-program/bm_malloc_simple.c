af0c9ce29539cba8ee4c118a326b081339c1e7ba
StanPlatinum <lpjlwjlpjlwj@126.com>
2019-08-29 22:30:47 -0400

    puts works

--- loader_v1/target-program/bm_malloc_simple.c
+++ loader_v1/target-program/bm_malloc_simple.c
@@ -9,10 +9,10 @@ void enclave_main()
     char* buf = (char*)malloc(0x8000);
     if (buf) {
         sprintf(buf, "The number is %d, the string is %s, another number is 0x%llx\n\0", 137, "\"A long long long string\"\0", 0x123456789101112LLU);
-        //puts(buf);
+        puts(buf);
         free(buf);
     } else {
-        //puts("buf is NULL");
+        puts("buf is NULL");
     }
     enclave_exit();
 }
