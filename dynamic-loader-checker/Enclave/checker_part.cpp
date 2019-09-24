da5aa82502dbdf7925cde9f2919c0c04248b406d
Weijie Liu <lpjlwjlpjlwj@126.com>
2019-09-23 23:02:11 -0400

    add find_cmp_imm(...)

--- dynamic-loader-checker/Enclave/checker_part.cpp
+++ dynamic-loader-checker/Enclave/checker_part.cpp
@@ -49,6 +49,31 @@ static int find_memory_write(csh ud, cs_mode, cs_insn *ins)
 	return exist;
 }
 
+/* Weijie: if the return value is 1, then it means that this insn[j] is writting memory */
+static int find_cmp_imm(csh ud, cs_mode, cs_insn *ins)
+{
+	cs_x86 *x86;
+	int i, exist = 0;
+
+	if (ins->detail == NULL)	return -2;
+	//Weijie: returning -2 means this insn[j] is kind of "data" instruction
+
+	x86 = &(ins->detail->x86);
+	if (x86->op_count == 0)		return -1;
+	//Weijie: returning -1 means this insn[j] has no oprand
+	
+	// traverse all operands
+	for (i = 0; i < x86->op_count; i++) {
+		cs_x86_op *op = &(x86->operands[i]);
+		//Weijie: returning 1 means this insn[j] has accessing imm
+		if ((int)op->type == X86_OP_IMM){
+			exist++;
+			return 1;
+		}
+	}
+	return exist;
+}
+
 /* Weijie: used be an ecall of whole cs_open/disasm/close */
 int cs_disasm_entry(unsigned char* buf_test, Elf64_Xword textSize, Elf64_Addr textAddr) {
 	//PrintDebugInfo("Weijie: new enclave starts here.\n");
@@ -66,11 +91,15 @@ int cs_disasm_entry(unsigned char* buf_test, Elf64_Xword textSize, Elf64_Addr te
 		size_t j;
 		//Weijie: test
 		int if_memwt = 0;
+		int if_cmp_imm = 0;
 		for (j = 0; j < count; j++) {
 			PrintDebugInfo("0x%"PRIx64":\t%s\t\t%s\n", insn[j].address, insn[j].mnemonic, insn[j].op_str);
 			//Weijie: test
+			if_cmp_imm = find_cmp_imm(handle, CS_MODE_64, &insn[j]);
 			if_memwt = find_memory_write(handle, CS_MODE_64, &insn[j]);
 			if (if_memwt > 0)	PrintDebugInfo("\tThe above insn is writting memory!\n");
+			if (if_cmp_imm > 0)	PrintDebugInfo("\tThe above insn is accessing imm!\n");
+
 		}
 		cs_free(insn, count);
 	} else
