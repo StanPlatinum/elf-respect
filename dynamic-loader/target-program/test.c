b19894b83015c8c0aa34e0b2b5a5093018a52341
StanPlatinum <lpjlwjlpjlwj@126.com>
2019-09-24 23:25:43 -0400

    after instrumented test.c, segfault occurs, instead of 'Fine'

--- dynamic-loader/target-program/test.c
+++ dynamic-loader/target-program/test.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "enclave.h"
+
+int getNumber(int x){
+  return x * 2;
+}
 
 void enclave_main(){
   int x;
   int *x_add = &x;
 
-  x_add = x_add + 1000000000;
+  puts("Fine\n");
+
+  x_add = x_add + getNumber(500000);
+ 
   *x_add = 5;
+
+   enclave_exit();
 }
