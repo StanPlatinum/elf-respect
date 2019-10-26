9e71f567f041d7721f878572bd4be8e36d6a1e48
Weijie Liu <lpjlwjlpjlwj@126.com>
2019-10-26 13:06:20 -0400

    Update CFICheck.c

--- ShadowStackCFI/CFICheck.c
+++ ShadowStackCFI/CFICheck.c
@@ -5,6 +5,11 @@ void CFICheck(long long target)
     long long *CFICheckAddressPtr = 0x1FFFFFFFFFFFFFFF;
     int CFICheckAddressNum = 0x1FFFFFFF;
     int low = 0, high = CFICheckAddressNum - 1, mid;
+    
+    //Weijie: for debugging
+    char i_b[8];
+    char *ii_b = &i_b;
+    
     while (low <= high)
     {
         mid = low + (high - low) / 2;
@@ -19,7 +24,7 @@ void CFICheck(long long target)
         }
         else if (CFICheckAddressPtr[mid] > target)
         {
-            //ii_b = my_itoa(CFICheckAddressPtr[mid], , 10);
+            ii_b = my_itoa(CFICheckAddressPtr[mid], ii_b, 10);
             puts("larger!\n");
             high = mid - 1;
         }
