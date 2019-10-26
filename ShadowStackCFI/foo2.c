f6afbda2a9dfa36783d4647866b05249f06e9a51
Weijie Liu <lpjlwjlpjlwj@126.com>
2019-10-26 12:05:32 -0400

    Update foo2.c

--- ShadowStackCFI/foo2.c
+++ ShadowStackCFI/foo2.c
@@ -1,50 +1,9 @@
-#include <stdio.h>
 #include "enclave.h"
 
 int fun()
 {
     puts("----------1 in fun----------");
     return 1;
-}
-
-char *my_itoa(unsigned long long val, char *buf, unsigned radix)
-{
-    char   *p;             
-    char   *firstdig;      
-    char   temp;           
-    unsigned   digval;     
-    //puts("----------1 in my_itoa----------");
-    p = buf;
-    /*
-     * Weijie: use an unsigned 64 bit int as input
-    if(val <0)
-    {
-        *p++ = '-';
-        val = (unsigned long)(-(long)val);
-    }
-    */
-    firstdig = p; 
-    //puts("----------2 in my_itoa----------");
-    do{
-        digval = (unsigned)(val % radix);
-        val /= radix;
-       
-        if  (digval > 9)
-            *p++ = (char)(digval - 10 + 'a'); 
-        else
-            *p++ = (char)(digval + '0');      
-    }while(val > 0);
-   
-    *p-- = '\0 ';         
-    do{
-        temp = *p;
-        *p = *firstdig;
-        *firstdig = temp;
-        --p;
-        ++firstdig;        
-    }while(firstdig < p);  
-    //puts("----------3 in my_itoa----------");
-    return buf;
 }
 
 void enclave_main()
