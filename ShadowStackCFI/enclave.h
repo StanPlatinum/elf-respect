8d43922588e280b3a1988a3755f285acf3f9720f
Weijie Liu <lpjlwjlpjlwj@126.com>
2019-10-28 00:39:15 -0400

    Update enclave.h

--- ShadowStackCFI/enclave.h
+++ ShadowStackCFI/enclave.h
@@ -13,44 +13,4 @@ void ocall_open(char *name, int *fd);
 size_t get_line(int fd, char *buf);
 void push_gadget(unsigned long gadget);
 
-char *my_itoa(unsigned long long val, char *buf, unsigned radix)
-{
-    char   *p;             
-    char   *firstdig;      
-    char   temp;           
-    unsigned   digval;     
-    //puts("----------1 in my_itoa----------");
-    p = buf;
-    /*
-     * Weijie: use an unsigned 64 bit int as input
-    if(val <0)
-    {
-        *p++ = '-';
-        val = (unsigned long)(-(long)val);
-    }
-    */
-    firstdig = p; 
-    //puts("----------2 in my_itoa----------");
-    do{
-        digval = (unsigned)(val % radix);
-        val /= radix;
-       
-        if  (digval > 9)
-            *p++ = (char)(digval - 10 + 'a'); 
-        else
-            *p++ = (char)(digval + '0');      
-    }while(val > 0);
-   
-    *p-- = '\0 ';         
-    do{
-        temp = *p;
-        *p = *firstdig;
-        *firstdig = temp;
-        --p;
-        ++firstdig;        
-    }while(firstdig < p);  
-    //puts("----------3 in my_itoa----------");
-    return buf;
-}
-
 #endif
