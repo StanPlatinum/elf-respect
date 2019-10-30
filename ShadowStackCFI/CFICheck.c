2cbaa1672b9565cd2a54dbb0eaa54526da32552c
StanPlatinum <lpjlwjlpjlwj@126.com>
2019-10-30 16:36:48 -0400

    test

--- ShadowStackCFI/CFICheck.c
+++ ShadowStackCFI/CFICheck.c
@@ -1,73 +1,31 @@
-#include <stdio.h>
 
 void CFICheck(unsigned long long target)
 {
 	unsigned long long *CFICheckAddressPtr = 0x1FFFFFFFFFFFFFFF;
 	int CFICheckAddressNum = 0x1FFFFFFF;
-	int low = 0, high = CFICheckAddressNum, mid;
-    unsigned long long magic = 0x1;
-
-    //Weijie: for debugging
-	// char i_b[8];
-	// char *ii_b = &i_b;
-    // for (int i = 0; i < CFICheckAddressNum; i++)
-    // {
-    //     ii_b = my_itoa(i, ii_b, 10);
-    //     puts(ii_b);
-	// 	ii_b = my_itoa(CFICheckAddressPtr[i], ii_b, 16);
-	// 	puts(ii_b);
-    // }
-    
-	// puts("checking next indirect call...");
-
-	// char target_s[8];
-	// char *target_str = target_s;
-	// target_str = my_itoa(target, target_str, 16);
-	// puts("target: ");
-	// puts(target_str);
+	int low = 0, high = CFICheckAddressNum, mid = 0;
+	//unsigned long long magic = 0x1;
 
 	while (low <= high)
 	{
 		mid = low + (high - low) / 2;
-        // ii_b = my_itoa(mid, ii_b, 10);
-        // puts(ii_b);
-        // ii_b = my_itoa(low, ii_b, 10);
-        // puts(ii_b);
-        // ii_b = my_itoa(high, ii_b, 10);
-        // puts(ii_b);
-		// puts("Matching CFICheckAddressPtr[mid]: ");
-		// ii_b = my_itoa(CFICheckAddressPtr[mid], ii_b, 16);
-		// puts(ii_b);
-
 		if (mid > high)
 		{
 			break;
 		}
 		if (CFICheckAddressPtr[mid] > target)
 		{
-			//ii_b = my_itoa(CFICheckAddressPtr[mid], ii_b, 10);
-			//puts("larger!");
-			//puts("CFICheckAddress[mid]: ");
-			//puts(ii_b);
-			//puts("\n");
 			high = mid - 1;
 		}
 		else if (CFICheckAddressPtr[mid] < target)
 		{
-			//ii_b = my_itoa(CFICheckAddressPtr[mid], ii_b, 10);
-			//puts("smaller!");
-			//puts("CFICheckAddress[mid]: ");
-			//puts(ii_b);
-			//puts("\n");
 			low = mid + 1;
 		}
-        else
-        {
-            //puts("found it!");
+		else
+		{
 			return;
-        }
-        
+		}
+
 	}
-	//abort();
 	exit(-1);
 }
--- ShadowStackCFI/foo2.c
+++ ShadowStackCFI/foo2.c
@@ -1,69 +1,26 @@
-#include <stdio.h>
+//#include <stdio.h>
 
 #include "enclave.h"
 
 int fun()
 {
-    puts("----------1 in fun----------");
+    //puts("----------1 in fun----------");
     return 1;
 }
-
-#if 0
-char *my_itoa(unsigned long long val, char *buf, unsigned radix)
-{
-    char   *p;             
-    char   *firstdig;      
-    char   temp;           
-    unsigned   digval;     
-    puts("----------1 in my_itoa----------");
-    p = buf;
-    /*
-     * Weijie: use an unsigned 64 bit int as input
-    if(val <0)
-    {
-        *p++ = '-';
-        val = (unsigned long)(-(long)val);
-    }
-    */
-    firstdig = p; 
-    puts("----------2 in my_itoa----------");
-    do{
-        digval = (unsigned)(val % radix);
-        val /= radix;
-       
-        if  (digval > 9)
-            *p++ = (char)(digval - 10 + 'a'); 
-        else
-            *p++ = (char)(digval + '0');      
-    }while(val > 0);
-    puts("----------3 in my_itoa----------");
-
-    *p-- = '\0 ';         
-    do{
-        temp = *p;
-        *p = *firstdig;
-        *firstdig = temp;
-        --p;
-        ++firstdig;        
-    }while(firstdig < p);  
-    puts("----------4 in my_itoa----------");
-    return buf;
-}
-#endif
 
 void enclave_main()
 {
     int (*fp)(void);
     fp = fun;
-    puts("----------1----------");
+    //puts("----------1----------");
     int b = fp();
-    puts("----------2----------");
-    char i_b[8];
-    char *ii_b = &i_b;
-    puts("----------3----------");
-    ii_b = my_itoa(b, ii_b, 10);
-    puts("----------4----------");
-    puts(ii_b);
-    puts("success!");
+    //puts("----------2----------");
+    //char i_b[8];
+    //char *ii_b = &i_b;
+    //puts("----------3----------");
+    //ii_b = my_itoa(b, ii_b, 10);
+    //puts("----------4----------");
+    //puts(ii_b);
+    //puts("success!");
     enclave_exit();
 }
