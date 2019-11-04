0f4ce004fb888c5acddf89d992d2c3a840caf569
StanPlatinum <lpjlwjlpjlwj@126.com>
2019-11-03 20:34:39 -0500

    magic

--- dynamic-loader-checker/target-program/bmms.c
+++ dynamic-loader-checker/target-program/bmms.c
@@ -4,31 +4,13 @@
 //#include <stdlib.h>
 #include "enclave.h"
 
-char *bufuck;
-
-unsigned long* objs;
-unsigned long* objs_end;
-
-void init(){
-	bufuck[0] = '\0';
-}
 
 void enclave_main()
 {
-	unsigned int i;
-	objs = (unsigned long*)malloc(0x1000 * sizeof(unsigned long));
-	objs_end = (unsigned long*)malloc(0x1000 * sizeof(unsigned long));
-	/*
-	for (i = 0;i < 0x1000; ++i) {
-		size_t size = i % 5 + 5;
-		objs[i] = (unsigned long)malloc(size);
-		objs_end[i] = objs[i]+size;
-	}
-	*/
-
-	bufuck = (char *)malloc(1 * sizeof(char));
-	//init();
-	//free(buf);
+	//Weijie: the magic must be initialized
+	unsigned long magic = 0;
+	char* buf = (char*)malloc(0x80);
+	free(buf);
 	puts("test");
 	enclave_exit();
 }
